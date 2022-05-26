31b320e6883f2091bc340de64d25c047ef492575
Alduino <git@alduino.dev>
2022-05-26 16:01:24 +1000

    Only overwrite initial when not just changed

--- src/hooks/useCombinedValidateResult.ts
+++ src/hooks/useCombinedValidateResult.ts
@@ -15,10 +15,18 @@ export function useCombinedValidateResult(
     const wasLoadingWhenInitialUpdated = useRef(getInitialState(base, initial));
     const baseRef = useRef(base);
 
+    // true if it changed in this render
+    const initialJustChanged = useRef(false);
+
+    // true if it changed last render
+    const [hadJustChanged, setHadJustChanged] = useState(false);
+
     const [combined, setCombined] = useState(base);
 
     useEffect(() => {
         baseRef.current = base;
+
+        if (initialJustChanged.current) return;
 
         if (!wasLoadingWhenInitialUpdated.current && !base?.isLoading) {
             setCombined(base);
@@ -27,17 +35,44 @@ export function useCombinedValidateResult(
         if (wasLoadingWhenInitialUpdated.current && base?.isLoading) {
             wasLoadingWhenInitialUpdated.current = false;
         }
-    }, [baseRef, base, setCombined, wasLoadingWhenInitialUpdated]);
+    }, [
+        baseRef,
+        initialJustChanged,
+        base,
+        setCombined,
+        wasLoadingWhenInitialUpdated
+    ]);
 
     useEffect(() => {
         if (!initial) return;
+
+        // takes effect instantly
+        initialJustChanged.current = true;
+
+        // only takes effect next render
+        setHadJustChanged(true);
 
         setCombined(initial);
 
         if (baseRef.current?.isLoading) {
             wasLoadingWhenInitialUpdated.current = true;
         }
-    }, [initial, setCombined, baseRef, wasLoadingWhenInitialUpdated]);
+    }, [
+        initial,
+        initialJustChanged,
+        setHadJustChanged,
+        setCombined,
+        baseRef,
+        wasLoadingWhenInitialUpdated
+    ]);
+
+    useEffect(() => {
+        if (!hadJustChanged) return;
+
+        // clear after it has been on for one render
+        initialJustChanged.current = false;
+        setHadJustChanged(false);
+    }, [hadJustChanged, setHadJustChanged]);
 
     return combined ?? base;
 }
