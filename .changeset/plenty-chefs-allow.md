406614fe1da90a4d7bebe94a8cf55c924319aadd
Zach Barham <git@alduino.dev>
2023-11-05 13:25:54 +1000

    Add component to disable validation updates (#9)

---
+++ .changeset/plenty-chefs-allow.md
@@ -0,0 +1,5 @@
+---
+"@radiantguild/form-contexts": minor
+---
+
+Added a `<NoValidationUpdates>` component that disables any validation update calls in its children. See `useValidationUpdate`’s docs for more info.
--- src/hooks/useValidationUpdate.ts
+++ src/hooks/useValidationUpdate.ts
@@ -1,18 +1,25 @@
 import {ValidateResult} from "@radiantguild/yoogi";
 import {useContext, useEffect} from "react";
 import {ValidationSetterContext} from "~/contexts/ValidationSetterContext";
+import {NoValidationUpdateContext} from "~/contexts/NoValidationUpdateContext";
 
 /**
- * Sets the validation result of this input so that `useValidation` can read it
+ * Sets the validation result of this input so that `useValidation` can read it.
+ *
+ * If a composite form field includes sub-fields with `useValidationUpdate` (or `useInputState`),
+ * the composite form field should wrap those elements in `<NoValidationUpdates>` to prevent clashing updates.
  *
  * @see ValidationProvider
  * @see useValidation
+ * @see NoValidationUpdates
  */
 export default function useValidationUpdate(result: ValidateResult | null): void {
     const setValidateResult = useContext(ValidationSetterContext);
+    const disabled = useContext(NoValidationUpdateContext);
 
     useEffect(() => {
+        if (disabled) return;
         setValidateResult?.update(result);
-    }, [setValidateResult, result]);
+    }, [disabled, setValidateResult, result]);
 }
 
